56708c6238da1cdb056138dafe5f0879b2af5cad
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-01-20 23:46:30 +0000

    Operaciones para obtener datos esstudiantes, cursos y estudiante por id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,11 +1,28 @@
 
 import { IResolvers } from 'graphql-tools';
+import { database } from '../data/data.store';
 
 const query: IResolvers = {
     Query: {
-        estudiantes(): string {
-            return 'lista de estudiantes';
-        }
+        estudiantes(): any {
+            return database.estudiantes;
+        },
+        estudiante(__: void, { id }): any {
+            const resultado = database.estudiantes.filter(estudiante => estudiante.id === id)[0];
+            if (!resultado) {
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado el estudiante con el ID ${id}`,
+                    email: '',
+                    courses: []
+                };
+            } else {
+                return resultado;
+            }
+        },
+        cursos(): any {
+            return database.cursos;
+        },
     }
 }
 
---
+++ src/resolvers/type.ts
@@ -0,0 +1,18 @@
+
+import { IResolvers } from 'graphql-tools';
+import _ from 'lodash';
+import { database } from '../data/data.store';
+
+const type: IResolvers = {
+    Estudiante: {
+        courses: parent => {
+            const cursosLista: Array<any> = [];
+            parent.courses.map((curso: string) => {
+                cursosLista.push(_.filter(database.cursos, ['id', curso])[0])
+            });
+            return cursosLista;
+        }
+    }
+}
+
+export default type;
