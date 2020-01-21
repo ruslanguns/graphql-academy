7daff7231f834b693985997cb2693d428a3335d6
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-01-21 00:31:20 +0000

    Mutation: crear un nuevo curso

--- src/resolvers/resolversMap.ts
+++ src/resolvers/resolversMap.ts
@@ -2,9 +2,11 @@
 import { IResolvers } from 'graphql-tools';
 import query from './query';
 import type from './type';
+import mutation from './mutation';
 
 const resolversMap: IResolvers = {
     ...query,
+    ...mutation,
     ...type
 }
 
